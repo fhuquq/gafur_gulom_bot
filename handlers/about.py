64900e177d6517cd03ab14c425bd5c23150402b8
fhuquq <fhuquq@gmail.com>
2026-04-20 17:00:17 +0500

    Update about.py

--- handlers/about.py
+++ handlers/about.py
@@ -150,108 +150,108 @@ Zaminday vazminu
 Mehnatkash, mushfiq
 Istagan narsangni tayerlaguvchi
 Xalq bor- otang bor.
-Cho‘chima, jigarim,
-O‘z uyingdasan,
+Cho'chima, jigarim,
+O'z uyingdasan,
 Bu yerda
-Na g‘urbat, na ofat, na g‘am.
+Na g'urbat, na ofat, na g'am.
 Bunda bor: harorat, muhabbat, shafqat
-Va mehnat nonini ko‘ramiz baham,
+Va mehnat nonini ko'ramiz baham,
 Sen yetim emassan,
 Uxla, jigarim.
 
 Bu yerda muzlagan
 Ajdar halqumli
-Vahshat to‘plarining
-Qahqahasi yo‘q.
+Vahshat to'plarining
+Qahqahasi yo'q.
 Bu yerda — odamzod
 shaklida yurgan
 manfur, yukumli, maraz namoyishi
 topolmas huquq.
 
-Ulug‘ jang kunidir,
+Ulug' jang kunidir,
 Jangki, beomon,
 Lashkar degan axir
-Bexatar bo‘lmas,
-Otang o‘lgan bo‘lsa,
-Qayg‘urma, qo‘zim,
-Ko‘zim usti
+Bexatar bo'lmas,
+Otang o'lgan bo'lsa,
+Qayg'urma, qo'zim,
+Ko'zim usti
 Minnating boshimga durra.
-Shu sog‘lom havoda
+Shu sog'lom havoda
 Salomat-omon,
-Xo‘rsinmay, erkinlab
+Xo'rsinmay, erkinlab
 Ola bera nafas.
 
-Ey, ulug‘ naslimning
+Ey, ulug' naslimning
 Yuragi, joni,
 Kiprigingga ilinmas
-Yig‘idan zarra.
+Yig'idan zarra.
 Sen yetim emassan,
 Uxla, jigarim.
 
 Yetimlik nimadir-
-Bizlardan so‘ra,
-O‘ninchi yillarning
+Bizlardan so'ra,
+O'ninchi yillarning
 Sargardonligi
 Isitma aralash
-Qo‘rqinch tush kabi
-Xayol ko‘zgusidan
+Qo'rqinch tush kabi
+Xayol ko'zgusidan
 Uchmaydi sira.
 
-Men yetim o‘tganman,
+Men yetim o'tganman,
 Oh, u yetimlik…
 Voy, bechora jonim,
 Desam arziydi.
 Boshimni silashga
-Bir mehribon qo‘l,
-Bir og‘iz shirin so‘z
+Bir mehribon qo'l,
+Bir og'iz shirin so'z
 Nondek arzanda.
 Men odam edim-ku,
 Inson farzandi…
 Sen yetim emassan,
 Uxla jigarim.
 
-Ko‘zimga o‘yquning
+Ko'zimga o'yquning
 Mudroq ovchisi
-To‘rin solmasdan, —
+To'rin solmasdan, —
 (Birinchi gudokka
 Ikki soat bor)-
 Otalik hissining
 Bebaho, laziz
-To‘lqinlar ichra
-G‘arq bo‘lib ketib,
+To'lqinlar ichra
+G'arq bo'lib ketib,
 Aziz boshing ustida
 Termulmakdaman,
 Sen yetim emassan,
 Uxla, jigarim.
 
-Nega cho‘chib tushding,
-Murg‘ak xayolingga
+Nega cho'chib tushding,
+Murg'ak xayolingga
 Nimalar keldi?
 Balki Odess dahshati,
 Kerch fojiasi,
 Yovvoyi maxluqlar,
-Qonxo‘r vahshiylar…
+Qonxo'r vahshiylar…
 Mammasi kesilgan
-Sho‘rlik onangning
+Sho'rlik onangning
 Pajmurda gavdasi
-Ko‘zing o‘ngida
+Ko'zing o'ngida
 Butun dahshati-la
 Aks etar endi.
 
-Onang xo‘rlandimi,
-Otang o‘ldimi,
+Onang xo'rlandimi,
+Otang o'ldimi,
 Sen yetim qoldingmi,
-Qayg‘urma, qo‘zim,
-Ko‘zim usti,
+Qayg'urma, qo'zim,
+Ko'zim usti,
 Minnating boshimga durra.
 
 Ota-onasining
 Tayini ham yuq,
-Sut ko‘r qilgur, haromi
+Sut ko'r qilgur, haromi
 Gitler oqpadar
 Farzandning qadrini
 Qayerdan bilsin?
-Bir qo‘ng‘iz mo‘ylovli,
+Bir qo'ng'iz mo'ylovli,
 Baroq soch mal’un,
 Jigar rang bir mundir
 Istagi uchun,
@@ -265,20 +265,20 @@ Kezar darbadar?!
 Axir, juda yaqin
 Qonli intiqom,
 Alhazar, alhazar,
-Ko‘pirar tomirlarda qon,
+Ko'pirar tomirlarda qon,
 Va portlar hademay
-Bu o‘tli vulqon.
+Bu o'tli vulqon.
 
 Sen yetim emassan,
 Uxla, jigarim.
 Sen kulayotibsan,
 Balki bu kulgi
-So‘nggi oylar ichra
+So'nggi oylar ichra
 birinchi chechak.
 La’li labingdagi
-G‘uncha tabassum
+G'uncha tabassum
 Albat toleingga
-Muhr bo‘ladi
+Muhr bo'ladi
 Va bunda aks etar
 Porloq kelajak.
 Tong yaqin, tong yaqin,
@@ -291,11 +291,11 @@ Uxlamoqdasen.
 Lablaring shivirlar,
 Izlamokdalar
 Balki bir erkalash,
-Bir ona bo‘sa,
-Ulug‘ oilaning
-go‘dak farzandi,
-bilib qo‘y endi:
-Sen tezda ulg‘ayib,
+Bir ona bo'sa,
+Ulug' oilaning
+go'dak farzandi,
+bilib qo'y endi:
+Sen tezda ulg'ayib,
 Olam kezasan.
 Manglayda porlagan
 Toleing — quyosh,
@@ -303,7 +303,7 @@ Butun yer yuzini
 Qilur munavvar.
 Xaqorat yemrilur,
 Zulm yanchilur,
-Jahonda bo‘lurmiz
+Jahonda bo'lurmiz
 Ozod, muzaffar.
 Sen yetim emassan,
 Mening Jigarim!",
